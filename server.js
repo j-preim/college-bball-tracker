fc9b982b1bb42a9d86a1613acc4310d9c8363d89
j-preim <j.preimesberger@gmail.com>
2024-03-21 14:56:54 -0500

    server stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 // const cookieParser = require("cookie-parser");
 
-// const db = require('./config/connection');
+const db = require('./config/connection');
 
 // const routes = require('./routes');
 
