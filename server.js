1e12f6fddcae17603b671728f823acc0f1999a97
j-preim <j.preimesberger@gmail.com>
2024-03-21 15:33:55 -0500

    fixed server

--- server.js
+++ server.js
@@ -17,13 +17,12 @@ app.use(express.json());
 
 // NOTE: some build tools create a folder named "build" while others, like Vite, create one called "dist"
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "..", 'client/dist')));
+  app.use(express.static(path.join(__dirname,'dist')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve( __dirname, "..", 'client/dist/index.html'))
+    res.sendFile(path.resolve( __dirname,'dist/index.html'))
   })
 }
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
 });
-
