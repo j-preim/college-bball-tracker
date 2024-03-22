09739bd51eb6be5692f6dc186961569f4f6bd323
j-preim <j.preimesberger@gmail.com>
2024-03-21 21:19:15 -0500

    shadow tweaking

--- src/pages/Matchups.jsx
+++ src/pages/Matchups.jsx
@@ -13,7 +13,7 @@ export default function Matchups(props) {
   return (
     <>
     <div className="p-3">
-        <label htmlFor="dates"><h4 className="lato-regular">Select a day:&nbsp;</h4></label>
+        <label htmlFor="dates"><h4 className="lato-regular headline">Select a day:&nbsp;</h4></label>
         <select name="dates" value={selectedDay} onChange={handleInputChange}>
         {props.gameDates.map((date) => (
           <option value={date} key={date}>{date}</option>
