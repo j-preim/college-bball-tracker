ccce55da47e13b126ea0de6dffef6afa05e87e9d
j-preim <j.preimesberger@gmail.com>
2026-03-13 11:35:55 -0500

    fix deploy errors 3

--- server/server.js
+++ server/server.js
@@ -19,10 +19,10 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("dist"));
+  app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
   app.get("*", (_req, res) => {
-    res.sendFile(path.join("dist/index.html"));
+    res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
   });
 }
 
