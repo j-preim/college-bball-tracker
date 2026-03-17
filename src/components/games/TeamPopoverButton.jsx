c82c97106408fd5dd12276e4c420f0b38779441b
j-preim <j.preimesberger@gmail.com>
2026-03-16 22:57:10 -0500

    seed

--- src/components/games/TeamPopoverButton.jsx
+++ src/components/games/TeamPopoverButton.jsx
@@ -67,9 +67,9 @@ export default function TeamPopoverButton({ team, bracketRank }) {
       type="button"
       className="btn btn-link btn-sm p-0 text-decoration-none team-popover-button"
     >
-      <span className="badge rounded-pill text-bg-light border me-1">
+      <span className="seed">
         {team.seed ?? "-"}
-      </span>
+      </span> &nbsp;
       <span>{team.name}</span>
     </button>
   );
