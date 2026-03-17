9b2bc7939e236116f6081443c30931aeb4b03ad5
j-preim <j.preimesberger@gmail.com>
2026-03-17 01:12:13 -0500

    fonts

--- src/components/games/ListGames.jsx
+++ src/components/games/ListGames.jsx
@@ -57,7 +57,7 @@ export default function ListGames({
   return (
     <section className="mt-2">
       <div className="d-flex justify-content-between align-items-center mb-2">
-          <div className="text-body-secondary small">
+          <div className="text-body-secondary small hoverText">
             Hover over team names for more info
         </div>
 
