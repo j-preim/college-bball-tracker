91c72581876d6c343ffcfa92950b870a04b29f7d
j-preim <j.preimesberger@gmail.com>
2026-03-18 13:28:00 -0500

    mobile buttonn size 4

--- src/pages/Entries.jsx
+++ src/pages/Entries.jsx
@@ -53,9 +53,9 @@ function StatusBadge({ status }) {
     <span
       style={{
         display: "inline-block",
-        padding: "4px 10px",
+        padding: isMobile ? "2px 6px" : "4px 10px"
         borderRadius: 999,
-        fontSize: 12,
+        fontSize: isMobile ? 10 : 12
         fontWeight: 600,
         ...styles,
       }}
@@ -108,7 +108,6 @@ function PickHistory({ entryId, picks = [], onRemovePick, isMobile }) {
               >
                 <StatusBadge
                   status={pick.result}
-                  style={{ fontSize: 8, padding: "2px 6px" }}
                 />
                 <button
                   onClick={() => onRemovePick(entryId, pick.pickDate)}
